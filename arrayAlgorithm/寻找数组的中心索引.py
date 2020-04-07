348dfdd64ceadb5b2732e10d85dab077a9a4f6a1
mosson <136782643@qq.com>
2020-04-07 14:14:37 +0800

    至少是i 其他数字两倍的最大数

--- arrayAlgorithm/寻找数组的中心索引.py
+++ arrayAlgorithm/寻找数组的中心索引.py
@@ -18,58 +18,7 @@ nums = [1, 2, 3]
 解释: 
 数组中不存在满足此条件的中心索引。
 """
-
-# 解法1
-class Solution(object):
-    def pivotIndex(self, nums):
-        len_nums = len(nums)
-        if len_nums <= 0 or len_nums > 10000:
-            return -1
-        if max(nums) > 1000 or min(nums) < -1000:
-            return -1
-        for k,v in enumerate(nums):
-            left, right = nums[0:k], nums[k+1:len_nums]
-            if sum(left) == sum(right):
-                return k
-        return -1
-
-# num1 = [1, 7, 3, 8, 6, 5, 6, 8]
-# ret1 = Solution()
-# key1 = ret1.pivotIndex(num1)
-# print(key1)
-
-# # 解法2
-# class Solution2(object):
-#     def pivotIndex(self, nums):
-#         """
-#         :type nums: List[int]
-#         :rtype: int
-#         """
-#         len_nums = len(nums)
-#         if len_nums <= 0 or len_nums > 10000:
-#             return -1
-#         if max(nums) > 1000 or min(nums) < -1000:
-#             return -1
-#
-#         sum_list = sum(nums)
-#         left = 0
-#         for k, v in enumerate(nums):
-#             left += v
-#             if k+1 >= len_nums:
-#                 return -1
-#             elif (left * 2 + nums[k+1]) == sum_list:
-#                 return k
-#         return -1
-#
-# #
-# # num2 = [1, 7, 3, 8, 6, 5, 6, 8]
-# num2 = [-1,-1,-1,-1,-1,-1]
-# ret2 = Solution2()
-# key2 = ret2.pivotIndex(num2)
-# print(key2)
-
-# 解法三：
-class Solution(object):
+class Solution3(object):
     """
     时间复杂度：O(N)O(N)，其中 NN 是 nums 的长度。
     空间复杂度：O(1)O(1)，使用了 S 和 leftsum
